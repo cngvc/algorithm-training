645cf7d8b864f52bb582884f1c1e1aba9fa8b268
cg <congvc98@gmail.com>
2024-03-26 23:14:13 +0700

    coin-change

---
+++ coin-change.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/coin-change/
+# solution includes the concepts of dynamic programming
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        db = [sys.maxint] * (amount + 1)
+        db[0] = 0
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    db[i] = min(db[i], 1 + db[i - c])
+        return db[amount] if db[amount] < sys.maxint else -1
