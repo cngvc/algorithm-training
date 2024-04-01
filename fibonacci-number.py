a9d544ac96af2c030b03cf19ad1a2ec9b0100d84
cg <congvc98@gmail.com>
2024-04-01 23:55:41 +0700

    fibonacci-number

---
+++ fibonacci-number.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/fibonacci-number
+
+class Solution(object):
+    def fib(self, n):
+        if n <= 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
